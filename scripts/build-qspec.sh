5219c125445cfa6615d283ba173a9f2d37d508ec
jan@eluv.io <jan.falkin@eluv.io>
2018-11-01 13:08:09 -0700

    avmaster builds without toolchain

--- scripts/build-qspec.sh
+++ scripts/build-qspec.sh
@@ -7,55 +7,35 @@ set -Eeuo pipefail
 
 clang_bin=clang++
 
-if test "$GOPATH" == ""; then
-    echo "GOPATH is not set"
-    exit 1
-fi
-
-if test "$CGO_CPPFLAGS" == ""; then
-    echo "CGO_CPPFLAGS is not set. Set using init-cgoenv.sh"
-    exit 1
-fi
-
-extra_flags=""
 if test $# -eq 1; then
-    extra_flags="$1"
+    projroot="$1"
+else
+    echo "Must specify project root"
+    exit -1
 fi
 
 shared_flags="-I../include -Wall -emit-llvm -std=c++14 -fno-use-cxa-atexit"
 
-cd "$GOPATH"/bitcode
+cd $projroot/bitcode
 
 (
 cd avmaster
-$clang_bin -O0 -g $extra_flags $CGO_CPPFLAGS $shared_flags \
+$clang_bin -O0 -g $shared_flags \
 	 -c avmaster2000.cpp -o avmaster2000.imf.bc
 
-$clang_bin -O0 -g $extra_flags $CGO_CPPFLAGS $shared_flags \
+$clang_bin -O0 -g $shared_flags \
 	 -c adsmanager.cpp -o adsmanager.bc
 )
 
 (
-cd avlive
-$clang_bin -O0 $extra_flags $CGO_CPPFLAGS $shared_flags \
-    -c live_rtp.cpp -o avlive.rtp.bc
-)
-
-(
 cd submission
-$clang_bin -O0 $extra_flags $CGO_CPPFLAGS $shared_flags \
+$clang_bin -O0 $shared_flags \
     -c submission.cpp -o submission.bc
 )
 
 (
-cd video
-$clang_bin -O0 $extra_flags $CGO_CPPFLAGS $shared_flags \
-    -c video.cpp -o video.bc
-)
-
-(
 cd library
-$clang_bin -O0 $extra_flags $CGO_CPPFLAGS $shared_flags \
+$clang_bin -O0 $shared_flags \
     -c library.cpp -o library.bc
 )
 
