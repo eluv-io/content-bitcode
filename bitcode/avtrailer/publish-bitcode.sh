46ed1630be17b9e203d4d2565d7ee94d714a392f
Peter Tseng <peter@eluv.io>
2019-03-11 21:22:37 -0700

    Update avtrailer with changes to avmaster

--- bitcode/avtrailer/publish-bitcode.sh
+++ bitcode/avtrailer/publish-bitcode.sh
@@ -2,16 +2,17 @@
 set -Eeuxo pipefail
 
 
-url="http://q1.contentfabric.io"
-qlibid="ilib1"
-
+#url="http://q1.contentfabric.io"
+#qlibid="ilib1"
+url="http://localhost:8008"
+qlibid="ilibmXGCuPZKWSKac13T3dAguedhEbQ"
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
 
 header="-H Accept: application/json -H Content-Type: application/json"
 qwt=`curl -s -X POST $header $url/qlibs/$qlibid/q \
-  -d \{\"meta\":\{\"bitcode\":\"avtrailer2.bc\"\}\} \
+  -d \{\"meta\":\{\"bitcode\":\"avtrailer.bc\"\}\} \
   | jq -r .write_token`
 qphash=`curl -s -X POST $header $url/qlibs/$qlibid/q/$qwt/data \
   --data-binary @avtrailer.bc \
--- bitcode/avtrailer/publish-content-example.sh
+++ bitcode/avtrailer/publish-content-example.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -Eeuxo pipefail
 
-source /p/e/git/content-fabric/init-gopath.sh
-source /p/e/git/content-fabric/init-cgoenv.sh
+source /p/e/git/ws/content-fabric/init-gopath.sh
+source /p/e/git/ws/content-fabric/init-cgoenv.sh
 
-/p/e/git/content-fabric/bin/qfab_cli submit video \
+# Doesn't work anymore
+/p/e/git/ws/content-fabric/bin/qfab_cli submit video \
 --log-level debug \
---url http://q1.contentfabric.io \
---library ilib1 \
+--url http://localhost:8008 \
+--library ilibmXGCuPZKWSKac13T3dAguedhEbQ \
 --name "BBB Trailer" \
---description "No time to waste" \
+--description "BIG BUCK BUNNY!!!" \
 --source /p/e/demo/media/SampleVideo_720x480_30mb.mp4 \
 --image /p/e/demo/media/THUMBNAILS/bbb.png  \
---type hq__QmfJ84sRiDYo2HBm8eoVGHT4c7CHiDi82vGbH2XDT3Lc89 \
+--type hq__QmVYxxWXccDbvT6J9xLKHC6JPHCU8eZFBHjePwxXiCLDft \
 --scriptsdir /p/e/git/MP/ondemand \
 --caddr "0x17a35784f29dbee8084864d609e865333f5dd121"
